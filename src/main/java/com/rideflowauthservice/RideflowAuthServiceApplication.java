55bfa063ac6d0f7559e3fd8bd76eabf6dc66f459
Abhilash Panja <abhilashpanja7@gmail.com>
2026-08-20 15:32:33 +0530

    First commit

---
+++ src/main/java/com/rideflowauthservice/RideflowAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rideflowauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideflowAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RideflowAuthServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rideflowauthservice/RideflowAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rideflowauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideflowAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
