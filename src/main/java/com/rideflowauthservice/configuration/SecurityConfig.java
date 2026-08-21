f5ead7e2cbd8a5dd5fb75d1c4c86609c751a1196
Abhilash-Panja <abhilashpanja7@gmail.com>
2026-08-21 15:31:29 +0530

    Added Security Config

--- src/main/java/com/rideflowauthservice/configuration/SecurityConfig.java
+++ src/main/java/com/rideflowauthservice/configuration/SecurityConfig.java
@@ -1,14 +1,25 @@
 package com.rideflowauthservice.configuration;
 
+import com.rideflowauthservice.security.CustomAccessDeniedHandler;
+import com.rideflowauthservice.security.CustomAuthenticationEntryPoint;
+import com.rideflowauthservice.security.JwtAuthenticationFilter;
+import com.rideflowauthservice.service.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final CustomUserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
+    private final CustomAccessDeniedHandler accessDeniedHandler;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -16,8 +27,15 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 );
-
-
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder builder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        builder.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
+        return builder.build();
     }
 }
