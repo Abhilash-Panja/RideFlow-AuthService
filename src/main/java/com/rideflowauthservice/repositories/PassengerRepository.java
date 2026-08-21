ec5a252804ab6680c380330198944922f97097c6
Abhilash-Panja <abhilashpanja7@gmail.com>
2026-08-21 15:30:17 +0530

    Added Requried repos

--- src/main/java/com/rideflowauthservice/repositories/PassengerRepository.java
+++ src/main/java/com/rideflowauthservice/repositories/PassengerRepository.java
@@ -4,5 +4,8 @@ package com.rideflowauthservice.repositories;
 import com.rideflowauthservice.models.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PassengerRepository extends JpaRepository<Passenger,Long> {
+    Optional<Passenger> findPassengerByEmail(String email);
 }
