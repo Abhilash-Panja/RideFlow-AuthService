67ba9aa8e5e5a1c5c1a5ac32fb0ad72f7f7cc167
Abhilash-Panja <abhilashpanja7@gmail.com>
2026-08-21 05:51:12 +0530

    Introduced Models

---
+++ src/main/java/com/rideflowauthservice/models/Driver.java
@@ -0,0 +1,22 @@
+package com.rideflowauthservice.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Driver extends BaseModel{
+    private String driverName;
+    private  String licenceNumber;
+    @OneToMany(mappedBy = "driver")
+    List<Booking> bookingList=new ArrayList<>();
+}
